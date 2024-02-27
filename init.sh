844ba85b77ec07da3aec99b847cccedc2e585ccb
Alex <77504261+leshe4ka46@users.noreply.github.com>
2024-02-28 00:30:44 +0300

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sudo pacman -S zsh --noconfirm
+sudo pacman -S git --noconfirm
+sh -c "$(curl -fsSL https://install.ohmyz.sh/)"
+sudo pacman -S nano --noconfirm
+
+sed -i 's/ZSH_THEME="robushell"/ZSH_THEME="agnoster"/g' file.txt
