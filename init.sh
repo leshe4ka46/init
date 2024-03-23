f52f93e41b8f7a8475c4169591ca2d01fb507bce
Alex <77504261+leshe4ka46@users.noreply.github.com>
2024-03-23 09:00:03 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,5 +5,5 @@ sudo pacman -S git --noconfirm
 sh -c "$(curl -fsSL https://install.ohmyz.sh/)"
 sudo pacman -S nano --noconfirm
 
-sed -i 's/ZSH_THEME="robushell"/ZSH_THEME="agnoster"/g' ~/.zshrc
-soirce ~/.zshrc
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
+source ~/.zshrc
